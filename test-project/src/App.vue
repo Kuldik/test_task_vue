dc1476d9712e360f8140b0d379288fa1c23ba2b3
Kuldik <112172212+Kuldik@users.noreply.github.com>
2024-08-07 11:27:34 +0000

    Корректировка "Добавление компонентов, настройка логики и стилей"

--- test-project/src/App.vue
+++ test-project/src/App.vue
@@ -1,25 +1,20 @@
-/* eslint-disable */
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view>
+      <h1>
+        adf
+      </h1>
+    </router-view>
   </div>
 </template>
-/* eslint-disable */
+
 <style lang="scss" scoped>
+@import "./assets/scss/main.scss";
 #app {
   font-family: PTsans, inter, Arial, sans-serif;
   padding: 0 100px;
   color: $color-font-main;
   font-size: 14px;
   margin-bottom: 36px;
-  @include _1200 {
-    padding: 0 80px;
-  }
-  @include _1000 {
-    padding: 0 50px;
-  }
-  @include _840 {
-    padding: 0 16px;
-  }
 }
 </style>
